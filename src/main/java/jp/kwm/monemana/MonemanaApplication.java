56f5e669dba90a33d2bb8fba5a0f1efaac82899a
Yusuke Kawamitsu <m.kawa@i.softbank.jp>
2022-07-10 17:55:26 +0900

    最初のコミット

---
+++ src/main/java/jp/kwm/monemana/MonemanaApplication.java
@@ -0,0 +1,13 @@
+package jp.kwm.monemana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonemanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonemanaApplication.class, args);
+	}
+
+}
